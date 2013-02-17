28fa12ab9fd293222105508505d3ab8bf615fb82
Shane Holder <shane.holder@pobox.com>
2013-02-17 15:26:31 -0600

    more interesting test

--- demoTesting/test/firstTest.js
+++ demoTesting/test/firstTest.js
@@ -9,15 +9,15 @@ describe('Our first Unit Test', function () {
     answer.should.equal(2);
   });
 
-  it('should pass again', function () {
+  it('should fail', function () {
     var answer;
 
     answer = 1 + 1;
-    answer.should.equal(2);
+    answer.should.equal(3);
   });
 
   it('load a file', function(){
-     fs.readFile('../package.json', function (err, file) {
+     fs.readFile('../../package.json', function (err, file) {
     	var data  = JSON.parse(file.toString());
     	data.version.should.equal('0.0.0');
      });
